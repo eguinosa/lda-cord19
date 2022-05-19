89c3739202020b805f54153b847220b325e99b6c
Gelin Eguinosa <eguinosa@gmail.com>
2022-05-18 21:01:55 -0400

    Move 'progress_bar' function to 'extra_funcs.py'.

--- extra_funcs.py
+++ extra_funcs.py
@@ -1,5 +1,28 @@
 # Gelin Eguinosa Rosique
 
+from sys import stdout
+
+
+def progress_bar(progress, total):
+    """
+    Print on the console a bar representing the progress of the function that
+    called this method.
+    :progress: How many actions the program has already covered.
+    :total: The total amount of actions the program need to do.
+    """
+    # Values to print.
+    steps = progress * 40 // total
+    percentage = progress * 100 // total
+
+    # Print progress bar.
+    stdout.write('\r')
+    stdout.write("[%-40s] %03s%%" % ('=' * steps, percentage))
+    stdout.flush()
+
+    # Add Break at the end of the progress bar, if we are done.
+    if progress == total:
+        print()
+
 
 def big_number(number):
     """
